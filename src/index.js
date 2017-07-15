039d1c940fb2448294a795e192995ef660a6ac68
FelixGanz <felix.ganz@epitech.eu>
2017-07-15 16:53:11 +0200

    basic map, work in progress...

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Calendar from './calendar.js'
 import Slack from './slack.js'
 import Locker from './locker.js'
 import './style.css'
-import './canvas.js'
 
 class Board extends React.Component {
     render() {
@@ -24,9 +23,6 @@ class Board extends React.Component {
 	      <div className="col-md-5">
 		<div className="box text-center" id="map">
 		  <img src={require("./img/map.png")} alt="hub-map"/>
-		    <canvas id="canvas" width="500" height="1000">
-		      Sorry, your browser doesn't support the &lt;canvas&gt; element.
-		    </canvas>
 		</div>
 	      </div>
 	    </div>
