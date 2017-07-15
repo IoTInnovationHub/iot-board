a011e479c5f8ebd2c409aa0d58842152ce20f4b5
Michael Halfon <michael.halfon@epitech.eu>
2017-07-16 00:40:37 +0200

    temperatures ne sont plus en dur

--- src/rooms.js
+++ src/rooms.js
@@ -1,46 +1,54 @@
 import React from 'react';
 
 export default class Rooms extends React.Component {
-    render() {
-	return (
-	    <div className="col-md-12">
-		<div className="box jumbotron text-center" id="rooms">
-		Work In Progress...
-		<h3><b>Temperatures</b></h3>
-		<table className="table borderless">
-		<tr>
-		<td>
-		<table className="table borderless box">
-		<tr>
-		<td rowspan={2}>
-		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Weather-sun-clouds-rain.svg/200px-Weather-sun-clouds-rain.svg.png" height="80" />
-		</td>
-		<td>
-		Cocreer<br/>17.5 Celsius
-	    </td>
-		</tr>
-		</table>
-		</td>
-		</tr>
-
-	    	<tr>
-		<td>
-		<table className="table borderless box">
-		<tr>
-		<td rowspan={2}>
-		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Weather-sun-clouds-rain.svg/200px-Weather-sun-clouds-rain.svg.png" height="80" />
-		</td>
-		<td>
-		Echanger<br/>18.5 Celsius
-	    </td>
-		</tr>
-		</table>
-		</td>
-		</tr>
-
-	    </table>
-	      </div>
-	    </div>
-	);
+    constructor()
+    {
+	super();
+	this.state = {items:[]}
+	setInterval(function()
+		    {
+			this.update()
+		    }.bind(this), 5000)
     }
+
+    update()
+    {
+	return (
+	    fetch('https://innovation-hub.herokuapp.com/hubData/')
+ 		.then(result=>result.json())
+		.then(items=>this.setState({items}))
+	)
+    }
+
+    componentDidMount(){
+	this.update();
+    }
+  render() {
+    return (
+      <div className="col-md-12">
+	<div className="box jumbotron text-center" id="rooms">
+	  Work In Progres...
+	  <h3><b>Temperatures</b></h3>
+	    <table className="table borderless">
+	      {
+		this.state.items.map(item=>
+		  (item.category === 'Temperature')
+		  ? (<table className="table borderless box">
+		    <tr>
+		      <td rowspan={2}>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Weather-sun-clouds-rain.svg/200px-Weather-sun-clouds-rain.svg.png" height="80"/>
+		      </td>
+		      <td>
+			{item.room}<br/>{item.value} {item.unit}
+		      </td>
+		    </tr>
+		  </table>)
+		  : null
+		)
+	      }
+	    </table>
+	  </div>
+	</div>
+    );
+  }
 }
